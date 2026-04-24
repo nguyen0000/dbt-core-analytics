b8b2c111ea2bac3965074b8018a5005d15fe92f5
trung.nguyen0@hotmail.fr <trung.nguyen0@hotmail.fr>
2026-04-24 09:38:57 +0000

    added a column to calculate lifetime_value ( the total amount a customer has spent at jaffle shop)

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,8 +6,15 @@ select * from {{ref('stg_jaffle_shop__customers')}}
 
 orders as (
 
-
 select * from {{ref('stg_jaffle_shop__orders')}}
+    
+),
+
+payment as (
+
+select      O.customer_id,sum(amount) as lifetime_value
+from        {{ref('stg_jaffle_shop__orders')}} as O left join {{ref('stg_stripe__payment')}} as P on O.order_id = P.orderid
+group by    1
     
 ),
 
@@ -30,16 +37,18 @@ customer_orders as (
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        C.customer_id,
+        C.first_name,
+        C.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(lifetime_value,0) as lifetime_value
 
-    from customers
+    from customers as C
 
-    left join customer_orders using (customer_id)
+    left join customer_orders   using (customer_id)
+    left join payment as P          on C.customer_id = P.customer_id
 
 )
 
