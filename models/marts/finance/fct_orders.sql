61d3eb1eaad62fa6ab5356662d8cb578c3a308f4
trung.nguyen0@hotmail.fr <trung.nguyen0@hotmail.fr>
2026-04-24 10:16:42 +0000

    update fct_orders to keep only payment_statuts successful

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -2,3 +2,4 @@ select  order_id,
         customer_id,
         payment_amount
 from    {{ref('stg_jaffle_shop__orders')}}  left join {{ref('stg_stripe__payment')}}  using (order_id)
+where payment_status = 'success'
